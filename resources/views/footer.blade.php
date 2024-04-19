7af658bab13715be88ea72e4473965934a55f735
Jose Manuel Montes <jose.montes@greelow.com>
2024-04-19 14:55:13 -0300

    dashboard template - construct layout

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <strong>Toro ADVERTISING <a href="https://adminlte.io">Web app test</a>.</strong>
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 0.01
+    </div>
+</footer>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,38 @@
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('Restaurant APP','Dashboard') Login</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{ asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+  @yield('addCss')
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+
+  <div class="wrapper">
+    @include('navbar')
+
+    @include('sidebar')
+    
+    @yield('wrapper-content')
+    
+    @include('footer')
+  </div>
+
+</body>
+</html>
